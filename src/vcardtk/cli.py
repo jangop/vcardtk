02b8a1616ea0898482ceb26125bc52d8bd882b55
Jan Philip Göpfert <jgoepfert@techfak.uni-bielefeld.de>
2023-09-05 10:18:43 +0200

    Define command line interface

--- src/vcardtk/cli.py
+++ src/vcardtk/cli.py
@@ -1,18 +1,41 @@
 from pathlib import Path
 
+import click
 import tqdm
 from loguru import logger
 
 from .core import process_vcards
 
 
-@logger.catch()
+@click.command()
+@click.argument(
+    "input_directory", type=click.Path(path_type=Path, exists=True, file_okay=False)
+)
+@click.argument("output_directory", type=click.Path(path_type=Path, file_okay=False))
+@click.option(
+    "--max-photo-file-size",
+    type=int,
+    default=1_000,
+    help="Maximum photo file size in bytes",
+)
+@click.option(
+    "--max-photo-width",
+    type=int,
+    default=512,
+    help="Maximum photo width in pixels",
+)
+@click.option(
+    "--max-photo-height",
+    type=int,
+    default=512,
+    help="Maximum photo height in pixels",
+)
 def enter(
-    input_directory: Path = Path("/home/jgoepfert/tmp/in"),
-    output_directory: Path = Path("/home/jgoepfert/tmp/out"),
-    max_photo_file_size: int = 10000,
-    max_photo_width: int = 512,
-    max_photo_height: int = 512,
+    input_directory,
+    output_directory,
+    max_photo_file_size,
+    max_photo_width,
+    max_photo_height,
 ):
     # Configure logger.
     logger.remove()
