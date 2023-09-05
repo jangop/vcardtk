7b5e8170c5dc56b3a4188daf4b4cae2306053fce
Jan Philip Göpfert <jgoepfert@techfak.uni-bielefeld.de>
2023-09-05 23:09:36 +0200

    Prompt for region code

--- src/vcardtk/normalization/numbers.py
+++ src/vcardtk/normalization/numbers.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+import click
 import phonenumbers
 from loguru import logger
 
@@ -16,45 +17,6 @@ def normalize_dates(vcard):
                 logger.error(f"Invalid date format for {field}: {value}; {error}")
 
 
-def _guess_region(vcard) -> str:
-    # If there is an address, use the country code from there.
-    if "adr" in vcard.contents:
-        addresses = vcard.contents["adr"]
-        for address in addresses:
-            if "country" in address.params:
-                country = address.params["country"][0]
-                if len(country) == 2:
-                    return country.upper()
-                if country == "USA":
-                    return "US"
-                if country == "UK":
-                    return "GB"
-                if country == "Germany":
-                    return "DE"
-                if country == "Deutschland":
-                    return "DE"
-
-    # If there is an email address, use the top-level domain from there.
-    if "email" in vcard.contents:
-        addresses = vcard.contents["email"]
-        for address in addresses:
-            if "." in address.value:
-                tld = address.value.split(".")[-1]
-                if tld.lower() == "de":
-                    return "DE"
-
-    # If there is a URL, use the top-level domain from there.
-    if "url" in vcard.contents:
-        addresses = vcard.contents["url"]
-        for address in addresses:
-            if "." in address.value:
-                tld = address.value.split(".")[-1]
-                if tld.lower() == "de":
-                    return "DE"
-
-    return ""
-
-
 def normalize_phone_numbers(
     vcard,
     *,
@@ -68,28 +30,31 @@ def normalize_phone_numbers(
         numbers = vcard.contents[field]
         for number in numbers:
             original_number = number.value
-
-            try:
-                parsed_number = phonenumbers.parse(original_number)
-
-                formatted_number = phonenumbers.format_number(
-                    parsed_number, phone_number_format
-                )
-
-            except phonenumbers.phonenumberutil.NumberParseException as error:
-                logger.error(
-                    f"Invalid phone number format: {original_number}; "
-                    f"{error}; guessing region..."
-                )
-                guessed_region = _guess_region(vcard)
-                if guessed_region:
-                    logger.critical(f"Guessed region: {guessed_region}")
-                vcard.prettyPrint()
-            else:
-                if formatted_number != original_number:
-                    logger.info(
-                        f"Normalized phone number: {original_number} "
-                        f"-> {formatted_number}"
+            region = None
+            while True:
+                try:
+                    parsed_number = phonenumbers.parse(original_number, region)
+                except phonenumbers.phonenumberutil.NumberParseException as error:
+                    logger.error(
+                        f"Invalid phone number format: {original_number}; {error}"
                     )
-                # Replace value.
+                    vcard.prettyPrint()
+                    region = click.prompt(
+                        "Enter region code",
+                        type=str,
+                        default=fallback_region,
+                        value_proc=lambda x: x.upper(),
+                    )
+                else:
+                    break
+
+            formatted_number = phonenumbers.format_number(
+                parsed_number, phone_number_format
+            )
+
+            if formatted_number != original_number:
+                logger.info(
+                    f"Normalized phone number: {original_number} "
+                    f"-> {formatted_number}"
+                )
                 number.value = formatted_number
